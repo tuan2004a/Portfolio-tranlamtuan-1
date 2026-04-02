bc7f97e7cb1c192ae1fadc7d9e7651eec69105d6
tuan2004a <bi03112004@gmail.com>
2026-04-02 20:03:58 +0700

    refactor: remove unused scrollToId from SideBar component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+			"@next/next/no-img-element": "off",
+		},
+	},
 ];
 
 export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,15 @@ import SideBar from "../components/layout/SideBar";
 import ProfessionalSkills from "../components/professionalSkills/Index";
 import Project from "../components/project/Index";
 import Contact from "../components/contact/Index";
-import { useSmoothScroll } from "../hook/useSmoothScroll";
 import Backround from "../components/Backround";
 import ButtonMenu from "../components/ButtonMenu";
 
 export default function Home() {
-	const scrollToId = useSmoothScroll();
 
 	return (
 		<div className="h-[100vh] w-full p-3.5 md:p-5 lg:p-7 xl:p-10 !gap-7 xl:!gap-10 flex max-lg:flex-col lg:overflow-hidden max-lg:overflow-y-scroll max-lg:overflow-x-hidden">
 			<div className="sidebarAnimationLoad z-50 lg:w-1/3 uk-height-1-1">
-				<SideBar scrollToId={scrollToId} />
+				<SideBar />
 			</div>
 			<main className="MainAnimationLoad z-50 scrollPageHidden lg:w-2/3 lg:overflow-y-scroll lg:overflow-x-hidden">
 				<section id="introduce" className="bg-[#191919] p-3.5 md:p-5 lg:p-7 xl:p-10 uk-width rounded-xl ">
