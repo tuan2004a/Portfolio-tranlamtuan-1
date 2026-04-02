c3e6f0611641123cdae36cbd82b61d6c6874227e
tuan2004a <bi03112004@gmail.com>
2026-04-02 20:31:33 +0700

    refactor: improve layout and styling in Card and Index components

--- src/components/project/Card.tsx
+++ src/components/project/Card.tsx
@@ -6,24 +6,24 @@ const Card = () => {
 	return (
 		<div className="uk-height-1-1">
 			{/* Giao diện desktop */}
-			<div className="grid grid-cols-4 max-sm:grid-cols-2 max-[813px]:grid-cols-3 lg:max-[1350px]:grid-cols-3 gap-5">
+			<div className="grid grid-cols-4 max-sm:grid-cols-2 max-[813px]:grid-cols-3 lg:max-[1350px]:grid-cols-3 gap-2 md:gap-5">
 				{/* <div className="max-sm:hidden grid grid-cols-4 gap-5 lg:max-[1060px]:gap-2 size-full"> */}
 				{dataProject.map((items, index) => (
-					<div key={index} className="h-[260px] w-[185px] relative">
+					<div key={index} className="h-65 w-full md:w-46.25 relative">
 						<div className="block size-full bg-black text-wrap relative rounded-lg overflow-hidden ">
-							<div className="overflow-hidden  uk-width-1-1 h-[100px] rounded-lg flex items-center justify-center relative">
-								<img src={items.image || ""} alt="" className="size-full object-center object-contain " />
+							<div className="overflow-hidden  uk-width-1-1 h-25 rounded-lg flex items-center justify-center relative">
+								<img src={items.image || ""} alt="" className="size-full object-center object-cover " />
 							</div>
-							<div className="!text-white !text-xs mt-3 mb-1 ml-1.75 border border-white px-1.5 py-px rounded-full w-fit">React.Js</div>
-							<h5 className="!my-0 !text-white !text-[17px] font-bold! line-clamp-2 !mx-2">{items.name}</h5>
+							<div className="text-white! text-xs! mt-3 mb-1 ml-1.75 border border-white px-1.5 py-px rounded-full w-fit">React.Js</div>
+							<h5 className="my-0! text-white! text-[17px]! font-bold! line-clamp-2 mx-2!">{items.name}</h5>
 							<div className="absolute bottom-2 flex gap-x-2 w-full px-2 z-50">
 								<a href={items.linkGithub || undefined} target="_blank">
-									<button className=" bg-[#3452ff] p-3.5 rounded-lg cursor-pointer">
+									<button className=" bg-[#3452ff] p-3 rounded-lg cursor-pointer">
 										<FaGithub className="size-5.5 text-white" />
 									</button>
 								</a>
 								<a href={items.LinkWeb || undefined} className="">
-									<button disabled={!items.LinkWeb} className="disabled:bg-gray-500 disabled:cursor-no-drop cursor-pointer bg-[#3452ff] text-white py-3 px-8 font-semibold rounded-md">
+									<button disabled={!items.LinkWeb} className="disabled:bg-gray-500 disabled:cursor-no-drop cursor-pointer bg-[#3452ff] text-white py-3 px-5 sm:px-8 font-semibold rounded-md">
 										Button
 									</button>
 								</a>
--- src/components/project/Index.tsx
+++ src/components/project/Index.tsx
@@ -3,17 +3,15 @@ import Card from './Card'
 
 const Index = () => {
     return (
-        <div className='uk-flex uk-flex-center uk-flex-column uk-width-1-1 uk-height-1-1'>
-            <h3 className='!m-0 !mb-5 xl:!mb-10 uk-width-1-1 !block !font-semibold !text-white relative after:absolute after:w-18 after:h-[3px] after:bg-[#3452ff] after:-bottom-1 after:left-0'>
-                Dự án
-            </h3>
-            <div className=' uk-flex uk-flex-center uk-width-1-1 uk-height-1-1'>
-                <div className=''>
-                    <Card />
-                </div>
-            </div>
-        </div>
-    )
+		<div className="uk-flex uk-flex-center uk-flex-column uk-width-1-1 uk-height-1-1">
+			<h3 className="m-0! mb-5! xl:mb-10! uk-width-1-1 block! font-semibold! text-white! relative after:absolute after:w-18 after:h-0.75 after:bg-[#3452ff] after:-bottom-1 after:left-0">Dự án</h3>
+			<div className=" uk-flex uk-flex-center uk-width-1-1 uk-height-1-1">
+				<div className="">
+					<Card />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Index
