141279ecd53af43b91e79c48bed1892cd99e323b
tuan2004a <bi03112004@gmail.com>
2026-04-02 20:07:30 +0700

    push

--- src/types/uikit.d.ts
+++ src/types/uikit.d.ts
@@ -1,12 +1,10 @@
 // src/types/uikit.d.ts
 declare module "uikit/dist/js/uikit.min.js" {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const UIkit: any;
 	export default UIkit;
 }
 
 declare module "uikit/dist/js/uikit-icons.min.js" {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const Icons: any;
 	export default Icons;
 }
